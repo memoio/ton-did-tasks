9f40ff48a0d2c4f8be25559c28ad28285afc258e
Allen2730 <1051072440@qq.com>
2025-05-14 17:39:41 +0800

    modify ton manifest

--- src/pages/api/tonconnect-manifest.js
+++ src/pages/api/tonconnect-manifest.js
@@ -2,8 +2,8 @@
 
 export default function handler(req, res) {
     res.status(200).json({
-        url: "https://datadid-ton_v2.memolabs.net",
-        name: "TON Vote",
-        iconUrl: "https://datadid-ton_v2.memolabs.net/memo.png"
+        url: "https://datadid-ton-v2.memolabs.net",
+        name: "Memo DID",
+        iconUrl: "https://datadid-ton-v2.memolabs.net/memo.png"
     });
 }
