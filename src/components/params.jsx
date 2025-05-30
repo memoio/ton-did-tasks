a78c94be947539acaf871215050e2bbc8eb6f213
Allen2730 <1051072440@qq.com>
2025-05-30 16:35:06 +0800

    test start params

--- src/components/params.jsx
+++ src/components/params.jsx
@@ -1,11 +1,34 @@
+export function base64UrlDecode(str) {
+    let base64 = str.replace(/-/g, '+').replace(/_/g, '/');
+    while (base64.length % 4) {
+        base64 += '=';
+    }
+    return decodeURIComponent(escape(atob(base64)));
+}
+
+export function base64UrlEncode(str) {
+    const base64 = btoa(unescape(encodeURIComponent(str)));
+    return base64
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+}
+
+
 export function decodeStartParams() {
     const startParam = window.Telegram?.WebApp?.initDataUnsafe?.start_param;
+    const params = {};
     if (startParam) {
         try {
-            const params = JSON.parse(decodeURIComponent(startParam || '{}'));
-            return params;
+            const paramsStr = base64UrlDecode(startParam || '{}');
+            paramsStr.split('&').forEach(pair => {
+                const [key, value] = pair.split('=');
+                if (key && value) params[key] = value;
+            });
         } catch (e) {
             return {};
         }
     }
+
+    return params;
 }
